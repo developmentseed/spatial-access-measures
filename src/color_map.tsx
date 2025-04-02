59a7b3fcc2d907b314e6837e21ddf128cbd4b5ea
Oliver Roick <oliver@roick.email>
2025-04-02 12:11:24 +1100

    Setup ESlint

--- src/color_map.tsx
+++ src/color_map.tsx
@@ -1,13 +1,16 @@
 import * as d3 from "d3";
 import {
-    Feature,
-    FeatureCollection
+  Feature,
+  FeatureCollection
 } from "geojson";
+import { ExpressionInputType, ExpressionSpecification } from "maplibre-gl";
 
-const getColorMap = (data: FeatureCollection, access_measure: string) => {
-    let minmax = d3.extent(data.features.map((item: Feature) => item.properties?.[access_measure]))
-    let scale = d3.scaleQuantize(minmax as unknown as number[], d3.schemeBuPu[9]);
-    return d3.range(minmax[0] as unknown as number, minmax[1] as unknown as number, 0.01).map((x) => [x, scale(x)])
-  };
+type ColorMap = [ExpressionSpecification | ExpressionInputType, ...(ExpressionSpecification | ExpressionInputType)[]]; // [string, number, ...ColorMap[]];
+
+const getColorMap = (data: FeatureCollection, access_measure: string): ColorMap => {
+  const minmax = d3.extent(data.features.map((item: Feature) => item.properties?.[access_measure]));
+  const scale = d3.scaleQuantize(minmax as number[], d3.schemeBuPu[9]);
+  return d3.range(minmax[0], minmax[1], 0.01).map((x) => [x, scale(x)]).map(([stop, color]) => [color, stop]).flat() as ColorMap;
+};
 
 export { getColorMap };
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,23 +1,29 @@
 import { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
+import { GeoJSONSource, Map as MapLibreMap } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "../styles/map.css";
-import * as arrow from "apache-arrow";
+import { Table } from "apache-arrow";
 import { bbox } from "@turf/bbox";
 import { getColorMap } from "../color_map";
 
+interface Props {
+  data?: Table;
+  access_measure: string
+}
+
 export default function Map(props: Props) {
-  const mapContainer = useRef<any>(null);
-  const map = useRef<any>(null);
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<MapLibreMap>();
   const [lng] = useState(-123.1120);
   const [lat] = useState(49.2488);
   const [zoom] = useState(11.5);
 
-  
-  useEffect(() => {
-    if (map.current) return; // stops map from intializing more than once
 
-    map.current = new maplibregl.Map({
+  useEffect(() => {
+    if (!mapContainer.current || map.current) return; // stops map from intializing more than once
+    const colorMap = getColorMap(JSON.parse(props.data?.toArray().map(Object.fromEntries)[0].feature_collection), props.access_measure);
+
+    map.current = new MapLibreMap({
       container: mapContainer.current,
       style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
       center: [lng, lat],
@@ -25,48 +31,52 @@ export default function Map(props: Props) {
       maxZoom: 14
     });
     map.current.on("load", function () {
-      map.current.addSource("da", {
+      map.current!.addSource("da", {
         type: "geojson",
         data: JSON.parse(props.data?.toArray().map(Object.fromEntries)[0].feature_collection),
       });
-      map.current.addLayer({
-        'id': 'da',
+      map.current!.addLayer({
+        "id": "da",
         "minzoom": 9.5,
-        'type': 'fill',
-        'source': 'da',
-        'layout': {},
-        'paint': {
-            'fill-color': {
-              property: props.access_measure,
-              stops: getColorMap(JSON.parse(props.data?.toArray().map(Object.fromEntries)[0].feature_collection), props.access_measure),
-            },
-            'fill-opacity': 0.8
+        "type": "fill",
+        "source": "da",
+        "layout": {},
+        "paint": {
+          "fill-color": [
+            "step",
+            ["get", props.access_measure],
+            ...colorMap,
+            colorMap[colorMap.length - 2]
+          ],
+          "fill-opacity": 0.8
         }
       });
-    })
+    });
   }, [lng, lat, zoom]);
 
   useEffect(() => {
-    if (!map.current.isSourceLoaded('da') || props.data==undefined) return;
+    if (!map.current || !map.current.isSourceLoaded("da") || props.data==undefined) return;
 
-    let boundaries = JSON.parse(props.data?.toArray().map(Object.fromEntries)[0].feature_collection);
-    let box = bbox(boundaries);
+    const boundaries = JSON.parse(props.data?.toArray().map(Object.fromEntries)[0].feature_collection);
 
-    map.current.getSource("da").setData(boundaries);
+    (map.current.getSource("da") as GeoJSONSource).setData(boundaries);
     map.current.setPaintProperty(
       "da",
       "fill-color",
       {
         property:props.access_measure,
         stops: getColorMap(boundaries,props.access_measure),
-      })
-    map.current.fitBounds(box, { padding: 30 });
+      });
+
+    const box = bbox(boundaries);
+    const [minX, minY, maxX, maxY] = box;
+    map.current.fitBounds([minX, minY, maxX, maxY], { padding: 30 });
 
   }, [props.data]);
 
 
   useEffect(() => {
-    if (!map.current.isSourceLoaded('da') || props.data==undefined) return;
+    if (!map.current || !map.current.isSourceLoaded("da") || props.data==undefined) return;
 
     map.current.setPaintProperty(
       "da",
@@ -74,7 +84,7 @@ export default function Map(props: Props) {
       {
         property:props.access_measure,
         stops: getColorMap(JSON.parse(props.data?.toArray().map(Object.fromEntries)[0].feature_collection),props.access_measure),
-      })
+      });
 
 
   }, [props.access_measure]);
@@ -85,9 +95,4 @@ export default function Map(props: Props) {
       <div ref={mapContainer} className="map" />
     </div>
   );
-}
-
-interface Props {
-  data: arrow.Table|undefined;
-  access_measure: string
 }
